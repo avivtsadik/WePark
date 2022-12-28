9a5a298f31b496a179ac1de79ff482647af4e586
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-12-28 18:25:21 +0200

    add drawer

---
+++ app/src/main/java/com/example/wepark/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wepark.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wepark.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
