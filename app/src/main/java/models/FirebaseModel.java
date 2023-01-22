c7dd35faa2b41b91f2cce6a6a757261c940c2367
avivtsadik <48044366+avivtsadik@users.noreply.github.com>
2023-01-23 00:22:41 +0200

    finish sync room and firebase

--- app/src/main/java/models/FirebaseModel.java
+++ app/src/main/java/models/FirebaseModel.java
@@ -9,6 +9,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
@@ -40,8 +41,10 @@ public class FirebaseModel {
         db.setFirestoreSettings(settings);
     }
 
-    public void getAllParkingLots(GetListener<List<Parking>> listener) {
-        db.collection("parkings").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+    public void getAllParkingLotsSince(Long since,GetListener<List<Parking>> listener) {
+        db.collection("parkings")
+                .whereGreaterThanOrEqualTo(Parking.LAST_UPDATED,new Timestamp(since,0))
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 List<Parking> list = new LinkedList<>();
