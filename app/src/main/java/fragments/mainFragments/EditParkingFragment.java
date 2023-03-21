bb66684121b9c0a30e486308b6cfe06f724c2d28
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-03-21 20:42:13 +0200

    123

--- app/src/main/java/fragments/mainFragments/EditParkingFragment.java
+++ app/src/main/java/fragments/mainFragments/EditParkingFragment.java
@@ -1,6 +1,5 @@
 package fragments.mainFragments;
 
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -8,10 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
@@ -19,27 +18,12 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
 import androidx.navigation.Navigation;
 
 import com.example.wepark.R;
 import com.squareup.picasso.Picasso;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import models.City;
-import models.CityMock;
 import models.Interfaces.OnActionDoneListener;
 import models.Parking;
 import models.ParkingMock;
@@ -50,7 +34,7 @@ public class EditParkingFragment extends Fragment {
     private Button addImageButton;
     private Button removeImageButton;
     private Spinner sizeSpinner;
-    private AutoCompleteTextView cityAutoComplete;
+    private TextView cityTextView;
     private ImageView imageView;
     private Parking parking;
     ActivityResultLauncher cameraAppLauncher;
@@ -75,26 +59,7 @@ public class EditParkingFragment extends Fragment {
                     }
                 });
     }
-    private void removeNonEnglishCityNames(List<String> cityNames) {
-        Iterator<String> iter = cityNames.iterator();
-        while (iter.hasNext()) {
-            String cityName = iter.next();
-            if (!hasEnglishChars(cityName)) {
-                iter.remove();
-            }
-        }
-    }
 
-    private boolean hasEnglishChars(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (Character.UnicodeBlock.of(c) == Character.UnicodeBlock.ARABIC
-                    || Character.UnicodeBlock.of(c) == Character.UnicodeBlock.HEBREW) {
-                return false;
-            }
-        }
-        return true;
-    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -104,7 +69,7 @@ public class EditParkingFragment extends Fragment {
         addImageButton = view.findViewById(R.id.addImageButton);
         removeImageButton = view.findViewById(R.id.removeImageButton);
         sizeSpinner = view.findViewById(R.id.sizespinner);
-        cityAutoComplete = view.findViewById(R.id.autoCompleteCity);
+        cityTextView = view.findViewById(R.id.cityTextView);
         imageView = view.findViewById(R.id.imageView);
         Button saveParkingBtn = view.findViewById(R.id.savebtn);
         Button cancelbtn = view.findViewById(R.id.cancelbtn);
@@ -119,18 +84,6 @@ public class EditParkingFragment extends Fragment {
         sizeSpinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.ParkingSize, R.layout.list_item);
         sizeSpinnerAdapter.setDropDownViewResource(R.layout.list_item);
         sizeSpinner.setAdapter(sizeSpinnerAdapter);
-
-        ArrayAdapter<String> cityAutoCompleteAdapter = new ArrayAdapter(getContext(), R.layout.list_item, new LinkedList());
-        cityAutoComplete.setDropDownHeight(600);
-        cityAutoComplete.setAdapter(cityAutoCompleteAdapter);
-
-        LiveData<List<City>> citiesLive = CityMock.instance.getCities();
-        citiesLive.observe(getViewLifecycleOwner(), citiesList -> {
-            List<String> cityNames = citiesList.stream().map(City::getName).collect(Collectors.toList());
-            removeNonEnglishCityNames(cityNames);
-            ArrayAdapter<String> cityAutoCompleteAdapter2 = new ArrayAdapter(getContext(), R.layout.list_item, cityNames);
-            cityAutoComplete.setAdapter(cityAutoCompleteAdapter2);
-        });
 
         addImageButton.setOnClickListener(view2 -> {
             cameraAppLauncher.launch(null);
@@ -149,7 +102,7 @@ public class EditParkingFragment extends Fragment {
             try {
                 String userId = LoginService.instance().getLoginService().getUserId();
                 String size = sizeSpinner.getSelectedItem().toString();
-                String city = cityAutoComplete.getText().toString();
+                String city = cityTextView.getText().toString();
 
                 if (!isUserAddImage) throw new Exception("Did not edit image");
 
@@ -185,31 +138,9 @@ public class EditParkingFragment extends Fragment {
     public void refreshFrame() {
         int sizePosition = sizeSpinnerAdapter.getPosition(parking.getSize());
         sizeSpinner.setSelection(sizePosition);
-        cityAutoComplete.setText(parking.getCity());
+        cityTextView.setText(parking.getCity());
         Picasso.get().load(parking.getAvatarUrl()).placeholder(R.drawable.avatar).into(imageView);
         isUserAddImage = true;
-    }
-
-    public String[] getCities() {
-        List<String> items = new ArrayList();
-        AssetManager am = getContext().getAssets();
-        try {
-            InputStream inputStream = am.open("cities.txt");
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            String str_line;
-
-            while ((str_line = buffer.readLine()) != null) {
-                str_line = str_line.trim();
-                if ((str_line.length() != 0)) {
-                    items.add(str_line);
-                }
-            }
-
-            return items.toArray(new String[items.size()]);
-
-        } catch (IOException e) {
-            return new String[0];
-        }
     }
 
 }
